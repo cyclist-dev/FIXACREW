e97bf6d7300a029c791f04900070d2508091aff2
cyclist-dev <moc.profissional@gmail.com>
2026-06-29 20:38:03 -0300

    fix:add DB_PORT to DNS for Railway

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 // como precisa ficar para o deploy funcionar
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_NAME', getenv('DB_NAME') ?: 'fixacrew');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASS') ?: '');
